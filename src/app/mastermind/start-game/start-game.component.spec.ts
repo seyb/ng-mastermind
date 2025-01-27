37150ac3e8fb580ca4989c010c9736ddb507f349
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2025-01-27 16:38:29 +0100

    Inits component tests using testing library

---
+++ src/app/mastermind/start-game/start-game.component.spec.ts
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/angular';
+import {StartGameComponent} from './start-game.component';
+import {expect} from 'vitest';
+
+describe('StartGameComponent', () => {
+  it('should display', async () => {
+    await render(StartGameComponent)
+    expect(screen.getAllByRole('button', { name: /Démarrer/i })).toHaveLength(1);
+  });
+});
---
+++ src/app/mastermind/start-game/start-game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-game',
+  imports: [],
+  templateUrl: './start-game.component.html',
+  styleUrl: './start-game.component.scss'
+})
+export class StartGameComponent {
+
+}
