dfad9d0fb9d14a0f4af7d9dca41f50de2dabe6ee
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2025-02-06 16:05:50 +0100

    Revert "Installs ngrx"
    This reverts commit 7baa471a63d2fc7e470176b7369f3b06a3e0f003.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideStore()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
