03db69a4066733c50b46fd5db922b22a47ec74dd
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2025-04-18 13:41:23 +0200

    Update package-lock.json with updated dependencies
    This commit upgrades multiple dependencies to newer versions, ensuring improved compatibility, performance, and security. Notable changes include updates to @analogjs packages, @angular-devkit packages, and associated dependencies like esbuild and marked. These updates aim to align with the latest releases and dependency requirements.

--- src/app/mastermind/start-game/start-game.component.ts
+++ src/app/mastermind/start-game/start-game.component.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
 import {startGameCommand} from '../../core/start-game.command';
+import {NgIf} from '@angular/common';
+import {GameId} from '../../core/game.model';
 
 @Component({
   selector: 'mastermind-start-game',
   standalone: true,
   template: `
-    {{ gameId }}
-    <button (click)="startGame()">Démarrer le jeu</button>
+    <div *ngIf="gameId">
+        game started with id:
+        {{ gameId }}
+    </div>
+    <button *ngIf="!gameId" (click)="startGame()">Démarrer le jeu</button>
   `,
+  imports: [
+    NgIf
+  ],
   styleUrl: './start-game.component.scss'
 })
 export class StartGameComponent {
-  gameId = 'new-game-id';
+  gameId : GameId | null = null;
 
   startGame() {
-    console.log('start');
-    this.gameId = startGameCommand().payload.id;
+    let gameStartedEvent = startGameCommand();
+
+    if (gameStartedEvent.type === 'gameStarted') {
+      this.gameId = gameStartedEvent.payload.id;
+    }
   }
 }
